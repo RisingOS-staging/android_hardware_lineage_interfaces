a8c9289173b61e54f3e2082efab5893e3a11a1bf
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-04-29 01:23:55 +0200

    interfaces: Add do_makefiles_update call for backend HIDLs
    This still doesn't unbreak previous call, but whatever
    
    Change-Id: I3267599cfc47a2d39ed480ebcc23514938a8f616

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -6,3 +6,8 @@ do_makefiles_update \
   "vendor.lineage:hardware/lineage/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
+
+do_makefiles_update \
+  "lineage.hardware:hardware/lineage/interfaces/_backend" \
+  "android.hardware:hardware/interfaces" \
+  "android.hidl:system/libhidl/transport"
